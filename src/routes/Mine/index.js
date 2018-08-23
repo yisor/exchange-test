0cf55ddc4ef4c1beb912c1829debdfc9b5990368
qichentao <qichentao@flybytrip.com>
2018-08-23 09:56:45 +0800

    我的页面修改

--- src/routes/Mine/index.js
+++ src/routes/Mine/index.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 import { intlShape } from 'react-intl';
 import styles from './MinePage.css';
 import { DocumentTitle } from 'components';
-import { candlestickOption, depthmapOption } from 'components/Charts/options';
 
-import { asyncComponent } from 'utils';
-const Candlestick = asyncComponent(() => import('components/Charts/Candlestick'));
-const Depthmap = asyncComponent(() => import('components/Charts/Depthmap'));
+import MineTop from './MineTop';
+import MineListView from './MineListView';
 
 class MinePage extends Component {
 
@@ -14,15 +12,12 @@ class MinePage extends Component {
 
   render() {
     const formatMsg = this.context.intl.formatMessage;
-    console.log(candlestickOption);
-
     return (
       <DocumentTitle title={formatMsg({ id: 'title.user' })}>
         <div className={styles.app}>
-          <header className={styles.appHeader} />
           <div className={styles.mainContent}>
-            <Candlestick option={candlestickOption} />
-            <Depthmap option={depthmapOption} />
+            <MineTop/>
+            <MineListView/>
           </div>
         </div>
       </DocumentTitle>
