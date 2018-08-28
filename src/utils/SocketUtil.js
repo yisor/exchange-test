470b4282eb6d2ec9b93e91e6b272b96d4639cb6a
liushenglin <liushenglin@flybycloud.com>
2018-08-28 09:40:58 +0800

    socket方法

---
+++ src/utils/SocketUtil.js
@@ -0,0 +1,95 @@
+let lockReconnect = false;  // 避免ws重复连接
+let closeSign = false;
+let ws = null;
+const wsUrl = 'ws://192.168.1.149:8082/exchange/appapi/ws';
+
+export const createWebSocket = () => {
+  try {
+    ws = new WebSocket(wsUrl);
+    initEventHandle(wsUrl);
+  } catch (err) {
+    reconnect(wsUrl);
+    console.error(err);
+  }
+};
+
+export const closeWebSocket = () => {
+  try {
+    closeSign = true;
+    console.log('主动关闭');
+    ws.close();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const initEventHandle = (wsUrl) => {
+  ws.onopen = () => {
+    // 心跳检测重置
+    heartCheck.reset().start();
+    console.log('连接成功!' + new Date().toUTCString());
+    ws.send({
+      'baseCoin': 'btc',
+      'depthStep': '1',
+      'event': 'SUB',
+      'klineTime': '5min',
+      'quoteCoin': 'usdt',
+      'symbol': 'btcusdt',
+      'token': 'string',
+      'type': 'TICKER'
+    }
+    );
+  };
+  ws.onclose = () => {
+    if (!closeSign) {
+      // reconnect(wsUrl);
+      console.log('连接关闭!' + new Date().toUTCString());
+    } else {
+      console.log('连接关闭2!' + new Date().toUTCString());
+      closeSign = false;
+    }
+  };
+  ws.onerror = () => {
+    reconnect(wsUrl);
+    console.log('连接错误!');
+  };
+  ws.onmessage = (event) => {
+    // 如果获取到消息，心跳检测重置
+    heartCheck.reset().start();
+    // 拿到任何消息都说明当前连接是正常的
+    console.log(event.data);
+  };
+};
+
+const reconnect = (url) => {
+  if (lockReconnect) return;
+  lockReconnect = true;
+  // 没连接上会一直重连，设置延迟避免请求过多
+  setTimeout(() => {
+    createWebSocket(url);
+    lockReconnect = false;
+  }, 3000);
+};
+
+// 心跳检测
+const heartCheck = {
+  timeout: 60 * 1000, // 60秒发一次心跳
+  timeoutObj: null,
+  serverTimeoutObj: null,
+  reset: function () {
+    clearTimeout(this.timeoutObj);
+    clearTimeout(this.serverTimeoutObj);
+    return this;
+  },
+  start: function () {
+    let self = this;
+    this.timeoutObj = setTimeout(() => {
+      // 这里发送一个心跳，后端收到后，返回一个心跳消息，
+      // onmessage拿到返回的心跳就说明连接正常
+      ws.send('ping');
+      self.serverTimeoutObj = setTimeout(() => { // 如果超过一定时间还没重置，说明后端主动断开了
+        ws.close();     // 如果直接执行reconnect 会触发onclose导致重连两次
+      }, self.timeout);
+    }, this.timeout);
+  }
+};
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,10 +2,12 @@ import EncryptUtils from './EncryptUtils';
 import TranslatUtil from './TranslatUtil';
 import asyncComponent from './AsyncComponent';
 import request from './request';
+import SocketUtil from './SocketUtil';
 
 export {
   request,
   asyncComponent,
   EncryptUtils,
   TranslatUtil,
+  SocketUtil
 };
